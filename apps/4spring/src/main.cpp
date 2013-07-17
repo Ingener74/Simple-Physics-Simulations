23cfb9a250aaac8488483d6fb139282e3b559c5d
Pavel Shnayder <pavel_shnayder@3divi.com>
2013-07-17 17:32:30 +0600

    4spring now display springs

--- apps/4spring/src/main.cpp
+++ apps/4spring/src/main.cpp
@@ -13,17 +13,46 @@
 
 #include <GL/glut.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// window
+uint16_t windowWidth = 1024, windowHeight = 768;
+
+
 gltools::ShaderProgram* sp;
+GLuint aPOS = 0;
+GLuint uMVP = 0;
+
+glm::mat4 projMat;
+glm::mat4 viewMat;
+float FOVy = 45.f, nearPlane = 0.1f, farPlane = 1000.f;
+
+GLfloat coords[] = {
+		 0.f,  0.f, 0.5f,
+		-1.f, -1.f,  0.f,
+
+		 0.f,  0.f, 0.5f,
+		-1.f,  1.f,  0.f,
+
+		 0.f,  0.f, 0.5f,
+		 1.f, -1.f,  0.f,
+
+		 0.f,  0.f, 0.5f,
+		 1.f,  1.f,  0.f
+};
+GLuint coordBuffer = 0;
 
 const char* caption =
 		"Material Point on 4 spring"
 		;
 
 const char* vertex_source =
-		"attribute vec2 coord2d;"
-		""
+		"attribute vec4 aPOS;"
+		"uniform   mat4 uMVP;"
 		"void main(void){ "
-		"	gl_Position = vec4(coord2d, 0.0, 1.0); "
+		"	gl_Position = uMVP * aPOS;"
 		"}"
 		;
 
@@ -37,11 +66,45 @@ void display(void){
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 	glClearColor(0.1f, 0.4f, 0.2f, 1.f);
 
+	sp->useProgram();
+
+	glBindBuffer(GL_ARRAY_BUFFER, coordBuffer);
+
+	glm::mat4 mvp = projMat * viewMat * glm::mat4();
+
+	glUniformMatrix4fv(uMVP, 1, GL_FALSE, glm::value_ptr(mvp));
+
+	glVertexAttribPointer(aPOS, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glEnableVertexAttribArray(aPOS);
+
+	glDrawArrays(GL_LINES, 0, 8);
+
+	glDisableVertexAttribArray(aPOS);
+
 	glutSwapBuffers();
 }
 
 void reshape(int w, int h){
 	glViewport(0, 0, w, h);
+	projMat = glm::perspective(FOVy, w / float(h), nearPlane, farPlane);
+}
+
+void init(){
+	gltools::StringShaderLoader ssl(vertex_source, fragment_shader);
+	sp = new gltools::ShaderProgram(&ssl);
+	aPOS = sp->getAttribute("aPOS");
+	uMVP = sp->getUniform("uMVP");
+
+	glGenBuffers(1, &coordBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, coordBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(coords), coords, GL_STATIC_DRAW);
+
+	projMat = glm::perspective(FOVy, windowHeight / float(windowWidth), nearPlane, farPlane);
+	viewMat = glm::lookAt(
+			glm::vec3(2.f, 3.f, 4.f),
+			glm::vec3(0.f, 0.f, 0.f),
+			glm::vec3(0.f, 0.f, 1.f)
+	);
 }
 
 int main( int argc, char* argv[] ){
@@ -50,18 +113,19 @@ int main( int argc, char* argv[] ){
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH);
+	glutInitWindowSize(windowWidth, windowHeight);
 	glutCreateWindow("Material point on four spring");
+
+	glutReshapeFunc(reshape);
+	glutDisplayFunc(display);
+	glutIdleFunc(display);
 
 	GLenum err = glewInit();
 	if(err != GLEW_OK){
 		gltools::ErrorLogger::getInstance()->put(gltools::ErrorLogger::FATAL_ERROR, "glew error");
 	}
 
-	gltools::StringShaderLoader ssl(vertex_source, fragment_shader);
-	sp = new gltools::ShaderProgram(&ssl);
-
-	glutReshapeFunc(reshape);
-	glutDisplayFunc(display);
+	void init();
 
 	glutMainLoop();
 
